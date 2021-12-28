8526ac0c478a2bdac378ac806980f13800f51976
Konrad (PC) <kolego199@gmail.com>
2021-12-28 02:04:33 +0100

    fix bugs in lucky numbers

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -33,16 +33,19 @@ app.get('/api/luckyNumbers', (req, res) => {
     try {
         db.collection("numbers").doc("1").get().then((doc) => {
             const data = doc.data();
-            const today = new Date();
-            const dataDate = new Date(data.date._seconds * 1000);
             // en-GB locale uses dd/mm/YYYY for short date notation
+            const today = new Date();
             const currentDateString = today.toLocaleDateString("en-GB");
-            const dataDateString = dataDate.toLocaleDateString("en-GB");
-          
+            if (!data) {
+                return res.status(500).json({ errorDescription: "500 Server Error: No lucky numbers data exists. The database document has not been created. "});
+            }
             // check if all the requirements for new lucky numbers are met
-            if ( (currentDateString !== dataDateString) && ![0, 6].includes(today.getDay()) && !data.freeDays.includes(currentDateString) ) {
+            const dataDate = new Date(data.date._seconds * 1000);
+            const dataDateString = dataDate.toLocaleDateString("en-GB");
+            let luckyNumbers = data.luckyNumbers;
+            if ((currentDateString !== dataDateString) && ![0, 6].includes(today.getDay()) && !data.freeDays.includes(currentDateString)) {
                 // new lucky numbers must be generated
-                const newNumbers = [];
+                luckyNumbers = [];
                 for (let i = 0; i < data.numberQuantity / 2; i++) {
                     // generate a random number between 1 and quantity of possible left numbers
                     let helperNumber = randomIntFromInterval(1, data.splitPoint - data.usedBeforeSplit.length);
@@ -57,7 +60,7 @@ app.get('/api/luckyNumbers', (req, res) => {
                         }
                     }
                     // add result
-                    newNumbers.push(randomNumber); 
+                    luckyNumbers.push(randomNumber); 
                     data.usedBeforeSplit.push(randomNumber);
                     // reset the array if all numbers have been used
                     if (data.usedBeforeSplit.length === data.splitPoint)
@@ -65,8 +68,8 @@ app.get('/api/luckyNumbers', (req, res) => {
                         data.usedBeforeSplit = [];
                     }
                     // same for the other half
-                    let helperNumber = randomIntFromInterval(1, data.maxNumber - data.splitPoint - data.usedAfterSplit.length);
-                    let randomNumber = data.splitPoint + 1;
+                    helperNumber = randomIntFromInterval(1, data.maxNumber - data.splitPoint - data.usedAfterSplit.length);
+                    randomNumber = data.splitPoint + 1;
                     while (helperNumber > 0) {
                         if (!data.usedAfterSplit.includes(randomNumber)) {
                             helperNumber--;
@@ -75,7 +78,7 @@ app.get('/api/luckyNumbers', (req, res) => {
                             randomNumber++;
                         }
                     }
-                    newNumbers.push(randomNumber); 
+                    luckyNumbers.push(randomNumber); 
                     data.usedAfterSplit.push(randomNumber);
                     if (data.usedAfterSplit.length === data.maxNumber - data.splitPoint)
                     {
@@ -83,7 +86,7 @@ app.get('/api/luckyNumbers', (req, res) => {
                     }
                 }
                 db.collection("numbers").doc("1").set({
-                    luckyNumbers: newNumbers,
+                    luckyNumbers: luckyNumbers,
                     date: admin.firestore.Timestamp.fromDate(today),
                     usedBeforeSplit: data.usedBeforeSplit,
                     usedAfterSplit: data.usedAfterSplit
@@ -92,7 +95,7 @@ app.get('/api/luckyNumbers', (req, res) => {
             // return the new or old data
             return res.status(200).json({
                 date: currentDateString,
-                luckyNumbers: newNumbers || data.luckyNumbers,
+                luckyNumbers,
                 excludedClasses: data.excludedClasses
             });  
         })
