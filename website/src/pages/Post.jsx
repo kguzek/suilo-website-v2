f62cdb6a98c72ff2410d5400bc8b76f9900a6e2f
Mikołaj Mrózek <m.mik311@gmail.com>
2022-01-24 17:18:27 +0100

    created post loading logic

--- website/src/pages/Post.jsx
+++ website/src/pages/Post.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { Outlet, useParams, useNavigate } from 'react-router-dom'
 
 const Post = ({ setPage }) => {
+    const [loaded, setLoaded] = useState(false)
+    const [currentPostData, setCurrentPostData] = useState({})
 
     let params = useParams();
     let navigate = useNavigate();
@@ -13,10 +15,21 @@ const Post = ({ setPage }) => {
     if (params.postID === undefined) {
         navigate("/aktualnosci");
         return null;
+    } else if (!loaded) {
+        /*
+           FETCH POST DATA FROM API AND SAVE IT TO 
+           AS STATE AND CHANGE STATE TO LOADED
+
+           fetch(`URL${params.postID}`).then((res)=>{
+               setCurrentPostData(res);
+               setLoaded(true);
+           });
+       */
+        return null; // LOADING SCREEN //
     } else {
         return (
             <div className="page-main">
-                <p>Post - {params.postID}</p>
+
             </div>
         );
     }
