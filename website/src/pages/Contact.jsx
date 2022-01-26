e2f1f3f8507b40825b17b775ba54d34b33597736
Konrad (PC) <kolego199@gmail.com>
2022-01-26 22:03:25 +0100

    fix formatting and remove compile-time warnings
    remove dependency array from useEffect(), etc.

--- website/src/pages/Contact.jsx
+++ website/src/pages/Contact.jsx
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import MetaTags from 'react-meta-tags';
-import MailBox from '../media/mail.svg'
+import React, { useState, useEffect } from "react";
+import MetaTags from "react-meta-tags";
+import MailBox from "../media/mail.svg";
 
 const Contact = ({ setPage }) => {
+  useEffect(() => {
+    setPage("contact");
+  });
 
-    useEffect(() => {
-        setPage("contact")
-    }, [])
+  return (
+    <div style={{ minHeight: "89vh" }}>
+      <MetaTags>
+        <title>
+          Kontakt | Samorząd Uczniowski 1 Liceum Ogólnokształcącego w Gliwicach
+        </title>
+        <meta
+          name="description"
+          content="Można się z nami skontaktować online poprzez Facebooka, Instagrama, Discorda, oraz E-mail, jak i również fizycznie w szkole. Więcej informacji na stronie. "
+        />
+        <meta property="og:title" content="Kontakt | SUILO Gliwice" />
+        <meta property="og:image" content="" /> {/* IMAGE TO BE ADDED */}
+      </MetaTags>
+      <p>Contact</p>
+    </div>
+  );
+};
 
-    return (
-        <div style={{ minHeight: "89vh" }}>
-            <MetaTags>
-                <title>Kontakt | Samorząd Uczniowski 1 Liceum Ogólnokształcącego w Gliwicach</title>
-                <meta name="description" content="Można się z nami skontaktować online poprzez Facebooka, Instagrama, Discorda, oraz E-mail, jak i również fizycznie w szkole. Więcej informacji na stronie. " />
-                <meta property="og:title" content="Kontakt | SUILO Gliwice" />
-                <meta property="og:image" content="" /> {/* IMAGE TO BE ADDED */}
-            </MetaTags>
-            <p>Contact</p>
-        </div>
-    );
-}
-
-
-export default Contact
+export default Contact;
