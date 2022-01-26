4e1be288ed53b8d2ab1168d2c11e50dbe2c5fc77
Konrad (PC) <kolego199@gmail.com>
2022-01-26 22:01:06 +0100

    add DEFAULT_IMAGE post constant

--- website/src/misc.js
+++ website/src/misc.js
@@ -9,10 +9,17 @@ export function conjugatePolish(value, base, suffix1, suffix2) {
   }
   const lastLetter = value.toString()[value.toString().length - 1];
   const lastDigit = parseInt(lastLetter);
-  if ([2, 3, 4].includes(lastDigit) && ![12, 13, 14].includes(Math.abs(value))) {
+  if (
+    [2, 3, 4].includes(lastDigit) &&
+    ![12, 13, 14].includes(Math.abs(value))
+  ) {
     return `${value} ${base}${suffix1}a`;
   }
   return `${value} ${base}${suffix2}`;
 }
 
+// Temporary API URL assignment
 export const API_URL = "http://localhost:5001/suilo-page/europe-west1/app/api";
+
+// Temporary image URL if an article has none specified
+export const DEFAULT_IMAGE = "https://i.stack.imgur.com/6M513.png";
