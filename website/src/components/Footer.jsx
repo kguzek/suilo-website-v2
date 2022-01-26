21e37035ab2f286a357a9964d75f3b44dd7f8fc9
Konrad (PC) <kolego199@gmail.com>
2022-01-26 20:33:17 +0100

    make footer show current year instead of "2022"

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -1,12 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const Footer = () => {
-    return (
-        <div style={{ width: "100vw", backgroundColor: "#434343", color: "white", fontWeight: "300", fontSize: "12px", display: "flex", justifyContent: "center", marginTop: "5em" }}>
-            <p style={{ padding: "0", margin: "9px" }}>
-                suilo.org 2022 © Wszystkie prawa zastrzeżone.
-            </p>
-        </div>
-    );
-}
+  return (
+    <div
+      style={{
+        width: "100vw",
+        backgroundColor: "#434343",
+        color: "white",
+        fontWeight: "300",
+        fontSize: "12px",
+        textAlign: "center",
+        justifyContent: "center",
+        marginTop: "5em",
+      }}
+    >
+      <p style={{ padding: "0", margin: "9px" }}>
+        suilo.org {new Date().getFullYear()} © Wszystkie prawa zastrzeżone.
+      </p>
+    </div>
+  );
+};
 export default Footer;
