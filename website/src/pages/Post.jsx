ae03e6826f85511dc0432b16cf6615af1bce5bc7
Konrad (PC) <kolego199@gmail.com>
2022-01-26 20:37:52 +0100

    remove dummy data; add "refresh" URL parameter
    GET /aktualnosci/post/xxx/?refresh=true
    this forces a refresh to the news post cache

--- website/src/pages/Post.jsx
+++ website/src/pages/Post.jsx
@@ -1,66 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import MetaTags from "react-meta-tags";
-import { PostCardPreview, fetchData } from "../components/PostCardPreview";
-import { conjugatePolish } from "../misc";
+import NotFound from "./NotFound";
+import { PostCardPreview, fetchNewsData } from "../components/PostCardPreview";
+import { conjugatePolish, API_URL } from "../misc";
 
-// Change API URL in production
-const API_URL = "http://localhost:5001/suilo-page/europe-west1/app/api/news/";
 const MAX_CACHE_AGE = 2; // hours
-
-const testDataMain = [
-  /*{
-    id: `ijsdfb32tew`,
-    title: `Adam Sarkowicz: "uczymy do ostatniego żywego ucznia" `,
-    textShort: `W dobie pandemi Koronaświrusa, Adam Sarkowicz wypowiada mocne słowa: "Będziemy prowadzili zajęcia szkolne do ostatniego żywego ucznia." Udanych Igrzysk i niech los zawsze wam sprzyja!`,
-    content: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. `,
-    date: new Date(),
-    photo: `https://www.sportslaski.pl/static/thumbnail/article/med/13452.jpg`,
-    views: `2137`,
-    alt: "",
-    postAuthor: "Mikołaj Mrózek",
-    imageAuthor: "Mikołaj Mrózek",
-  },
-  {
-    id: `sdf89ub8743`,
-    title: `SpeedDating edycja 2022 - informacje`,
-    textShort: `Nowa edycja SpeedDating'u już przednami, w tym poście znajdziecie wszystkie przydatne informacje dotyczące tegorocznej edycji wydarzenia.`,
-    date: new Date(),
-    photo: `https://images.unsplash.com/photo-1544911845-1f34a3eb46b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
-    views: `4326`,
-  },
-  {
-    id: `534879bjifsd`,
-    title: `PiS znowu atakuje polską edukację `,
-    textShort: `Co tu dużo mówić, w końcu żyjemy w Polsce.. Niemniej tutaj mamy dla was krótkie podsumownie aktualnych informacji dotyczących LexCzarnek i idiotyzmów polskiego obozu rządzącego. `,
-    date: new Date(),
-    photo: `https://images.unsplash.com/photo-1529107386315-e1a2ed48a620?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
-    views: `42069`,
-  },
-  {
-    id: `432fsdsdffd`,
-    title: `Fotorelacja z wycieczki do babiogórskiego parku narodowego`,
-    textShort: `W dobie pandemi Koronaświrusa, Adam Sarkowicz wypowiada mocne słowa: "Będziemy prowadzili zajęcia szkolne do ostatniego żywego ucznia." Udanych Igrzysk i niech los zawsze wam sprzyja!`,
-    date: new Date(),
-    photo: `https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80`,
-    views: `2137`,
-  },*/
-];
 
 const Post = ({ setPage }) => {
   const [loaded, setLoaded] = useState(false);
+  const [loadedNews, setLoadedNews] = useState(false);
   const [currentPostData, setCurrentPostData] = useState({});
   const [dataMain, setDataMain] = useState([]);
-  const [dataPrimary, setDataPrimary] = useState([]);
+  const [_dataPrimary, setDataPrimary] = useState([]);
   const [dataSecondary, setDataSecondary] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const params = useParams();
 
-  let params = useParams();
-  let navigate = useNavigate();
-
-  function updatePostData() {
+  function updatePostData(updateCache = false) {
+    /**Updates the post data with the data from the API JSON response and sets the 'loaded' status. */
     function processData(data) {
       if (!data) {
         console.log("ERROR: Could not retrieve post data.");
+        setCurrentPostData({ errorDescription: "Data doesn't exist." });
         return setLoaded(true);
       }
       const newCache = {
@@ -76,84 +38,91 @@ const Post = ({ setPage }) => {
       setCurrentPostData(data);
       setLoaded(true);
     }
-    // check if there is a valid news data cache
+    // check if there is a valid post data cache
     const cache = JSON.parse(localStorage.getItem(params.postID));
     if (cache) {
-      // check if the cache contains no error
-      if (cache.data && !cache.data.errorDescription) {
-        // check if the cache is younger than 24 hours old
-        const cacheDate = Date.parse(cache.date);
-        const dateDifferenceSeconds = (new Date() - cacheDate) / 1000;
-        if (dateDifferenceSeconds / 3600 < MAX_CACHE_AGE) {
-          console.log("Found existing cache for post data.", cache);
-          setCurrentPostData(cache.data);
-          return setLoaded(true);
+      if (!updateCache) {
+        // check if the cache contains no error
+        if (cache.data && !cache.data.errorDescription) {
+          // check if the cache is younger than 24 hours old
+          const cacheDate = Date.parse(cache.date);
+          const dateDifferenceSeconds = (new Date() - cacheDate) / 1000;
+          if (dateDifferenceSeconds / 3600 < MAX_CACHE_AGE) {
+            console.log("Found existing cache for post data.", cache);
+            setCurrentPostData(cache.data);
+            return setLoaded(true);
+          }
         }
       }
-      // the cache is too old
+      // remove the existing cache
       localStorage.removeItem(params.postID);
     }
 
-    const url = API_URL + params.postID;
-    fetch(url).then((res) => {
+    fetch(`${API_URL}/news/${params.postID}`).then((res) => {
       res.json().then(processData);
     });
   }
 
   useEffect(() => {
     setPage("news");
-    fetchData(1, setDataMain, setDataPrimary, setDataSecondary, () => {});
-    updatePostData();
-  }, [params]);
+    if (!loadedNews) {
+      fetchNewsData(setDataMain, setDataPrimary, setDataSecondary, setLoadedNews);
+    }
+    const forceRefresh = searchParams.get("refresh");
+    forceRefresh && setSearchParams({});
+    updatePostData(forceRefresh);
+  }, []);
 
-  if (params.postID === undefined) {
-    navigate("/aktualnosci");
-    return null;
-  } else if (!loaded) {
-    updatePostData()
+  if (!loaded) {
     return null; // LOADING SCREEN //
-  } else {
-    const newDate = new Date(currentPostData.date).toLocaleDateString("pl-PL", {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    });
-    const views = conjugatePolish(currentPostData.views || 0, "wyświetle", "ni", "ń")
-    return (
-      <div className="page-main">
-        <MetaTags>
-          <title>{currentPostData.title}</title>
-          <meta name="description" content={currentPostData.text} />
-          <meta property="og:title" content={currentPostData.title} />
-          <meta property="og:image" content="" /> {/* IMAGE TO BE ADDED */}
-        </MetaTags>
-        <div className="post-division">
-          <article>
-            <img
-              className="post-image"
-              src={currentPostData.photo}
-              alt={currentPostData.alt}
-            />
-            <p className="image-author">
-              Zdjęcie: {currentPostData.imageAuthor}
-            </p>
-            <p className="post-info">
-              <span style={{ fontWeight: "500" }}>{newDate}</span>
-              &nbsp;&nbsp;·&nbsp;&nbsp;{views}
-            </p>
-            <h1 className="article-title">{currentPostData.title}</h1>
-            <p className="article-content">{currentPostData.content}</p>
-          </article>
-          <div className="post-sidebar">
-            <PostCardPreview type="secondary" data={dataSecondary} />
-          </div>
-        </div>
-        <div className="main-grid">
-          <PostCardPreview type="main" data={dataMain} />
+  }
+  if (currentPostData.errorDescription) {
+    setPage("not_found")
+    return <NotFound setPage={setPage} msg="Post nie istnieje." />;
+  }
+  const newDate = new Date(currentPostData.date).toLocaleDateString("pl-PL", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+  const views = conjugatePolish(
+    currentPostData.views || 0,
+    "wyświetle",
+    "ni",
+    "ń"
+  );
+  return (
+    <div className="page-main">
+      <MetaTags>
+        <title>{currentPostData.title}</title>
+        <meta name="description" content={currentPostData.text} />
+        <meta property="og:title" content={currentPostData.title} />
+        <meta property="og:image" content="" /> {/* IMAGE TO BE ADDED */}
+      </MetaTags>
+      <div className="post-division">
+        <article>
+          <img
+            className="post-image"
+            src={currentPostData.photo}
+            alt={currentPostData.alt}
+          />
+          <p className="image-author">Zdjęcie: {currentPostData.imageAuthor}</p>
+          <p className="post-info">
+            <span style={{ fontWeight: "500" }}>{newDate}</span>
+            &nbsp;&nbsp;·&nbsp;&nbsp;{views}
+          </p>
+          <h1 className="article-title">{currentPostData.title}</h1>
+          <p className="article-content">{currentPostData.content}</p>
+        </article>
+        <div className="post-sidebar">
+          <PostCardPreview type="secondary" data={dataSecondary} fromPost={true} />
         </div>
       </div>
-    );
-  }
+      <div className="main-grid">
+        <PostCardPreview type="main" data={dataMain} fromPost={true} />
+      </div>
+    </div>
+  );
 };
 
 export default Post;
