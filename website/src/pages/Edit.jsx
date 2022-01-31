fc70850a8e2a25cc22c20341545be10479a2973c
Konrad (PC) <kolego199@gmail.com>
2022-01-31 01:57:16 +0100

    update 'edit' page hooks

--- website/src/pages/Edit.jsx
+++ website/src/pages/Edit.jsx
@@ -1,44 +1,37 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import MetaTags from 'react-meta-tags';
+import MetaTags from "react-meta-tags";
 
 const Edit = ({ setPage, logged, loginAction }) => {
-    let navigate = useNavigate()
-    useEffect(() => {
-        if (!logged) {
-            // console.log("edit nie zalogowano")
-            loginAction();
-            navigate("/")
-            setPage("home")
-            return;
-        } else {
-            // console.log("edit zalogowoano")
-            setPage("edit")
-            document.title = "Edycja kontentu | Samorząd Uczniowski 1 Liceum Ogólnokształcącego w Gliwicach";
-            return;
-        }
-    }, []) //INITIAL CALL
+  let navigate = useNavigate();
 
-    useEffect(() => {
-        if (!logged) {
-            // console.log("edit nie zalogowano")
-            // loginAction();
-            navigate("/")
-            setPage("home")
-            return;
-        } else {
-            // console.log("edit zalogowoano")
-            setPage("edit")
-            return;
-        }
-    }, [logged]) //CALL WHEN LOGGED HAVE CHANGED
+  useEffect(() => {
+    if (logged) {
+      setPage("edit");
+    } else {
+      navigate("/");
+      setPage("home");
+      loginAction();
+    }
+  }, [logged]);
 
-    return (
-        <div style={{ minHeight: "89vh" }}>
-            <p>Edit</p>
-        </div>
-    );
-}
+  return (
+    <div style={{ minHeight: "89vh" }}>
+      <MetaTags>
+        <title>
+          Edycja treści | Samorząd Uczniowski 1 Liceum Ogólnokształcącego w
+          Gliwicach
+        </title>
+        <meta
+          name="description"
+          content="Edycja zawartości strony Samorządu Uczniowskiego 1 Liceum Ogólnokształącego w Gliwicach."
+        />
+        <meta property="og:title" content="Edycja | SUILO Gliwice" />
+        <meta property="og:image" content="" /> {/* TODO: Add image */}
+      </MetaTags>
+      <p>Edit</p>
+    </div>
+  );
+};
 
-
-export default Edit
+export default Edit;
