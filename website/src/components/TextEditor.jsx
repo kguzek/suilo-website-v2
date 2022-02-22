6fccba307e1a73729bb68b0294adbf5484f6c473
Mikołaj Mrózek <m.mik311@gmail.com>
2022-02-22 17:46:24 +0100

    Create TextEditor.jsx

---
+++ website/src/components/TextEditor.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+const TextEditor = () => {
+    return (
+        <div >
+            text editor lol
+        </div>
+    )
+}
+
+export default TextEditor;
