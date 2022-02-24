4d096fab76a328c5e48f2fe8fb0d53d564cacb8d
Konrad (PC) <kolego199@gmail.com>
2022-02-24 23:26:34 +0100

    fix some dates bugging when timezone different

--- website/src/common.js
+++ website/src/common.js
@@ -9,21 +9,18 @@ String.prototype.replaceAll = function replaceAll(search, replacement) {
 /** Return an array consisting of the date's year, month (one-padded) and day of the month.
  * If no date is provided, defaults to using the current date.
  */
-function dateToArray(date) {
+export function dateToArray(date) {
   // don't use default function argument for 'date' as that is evaluated at compile time, not runtime.
   // at least that's how it works in other languages, I haven't tested it in JavaScript ;)
-  date === undefined && (date = new Date());
+  date ?? (date = new Date());
   return [date.getFullYear(), date.getMonth() + 1, date.getDate()];
 }
 
 /** Return the array's elements as a string separated by the '-' character. */
-function serialiseDateArray(dateArray = []) {
-  // OLD METHOD
-  // return dateArray.toString().replaceAll(",", "-");
-
-  // the Date.toJSON() function returns a string with format "yyyy-MM-ddThh:mm:ss.sssZ"
+export function serialiseDateArray(dateArray = []) {
+  // This formats the date as yyyy-m-d
+  const serialised = dateArray.toString().replaceAll(",", "-");
+  // This formats the date as yyyy-mm-dd
   // splitting the string at 'T' and returning the first element of the split only gets you "yyyy-MM-dd"
-  return new Date(dateArray).toJSON().split("T").shift();
+  return new Date(serialised).toISOString().split("T").shift();
 }
-
-module.exports = { dateToArray, serialiseDateArray };
