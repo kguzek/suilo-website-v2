15217dbbb9881f75e5bf3788ec9b79297a13b43e
Mikołaj Mrózek <m.mik311@gmail.com>
2022-03-09 14:15:08 +0100

    Changed NotFound page styling

--- website/src/pages/NotFound.jsx
+++ website/src/pages/NotFound.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect } from "react";
+import NotFoundGraphic from '../media/notfound.jsx'
+import { useLocation } from "react-router-dom";
 
 const NotFound = ({ setPage, msg }) => {
+  const path = useLocation().pathname
+
   useEffect(() => {
     setPage("not_found");
   });
+
   return (
-    <div style={{ minHeight: "89vh" }}>
-      <p>Nie znaleziono zasobu, którego szukasz.</p>
-      {msg && <small>{msg}</small>}
+    <div className="min-h-[87vh] w-11/12 flex flex-col justify-center">
+      <div className="w-full sm:w-10/12 md:w-7/12 lg:w-1/3 mx-auto -mt-10">
+        <NotFoundGraphic />
+      </div>
+      <p className="mx-auto text-2xl font-semibold text-text6 text-center mt-4 lg:mt-8">Nie znaleziono zasobu, którego szukasz.</p>
+      <p className="mx-auto text-[.9rem] text-center font-normal mt-3 md:mt-1 md:text-base text-text7">Podstrona <code className="bg-text7/25 py-[.2rem] px-[.4rem] rounded-md text-text6">{path}</code> nie istnieje :v</p>
     </div>
   );
 };
