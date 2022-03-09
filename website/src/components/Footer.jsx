43f5f782af102ecaf3fe2938077cb71a8385b863
Mikołaj Mrózek <m.mik311@gmail.com>
2022-03-09 22:05:14 +0100

    who asked?

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from "react";
+import DialogBox from "./DialogBox";
 
 const Footer = ({ isVisible }) => {
+  const [easterEgg, setEasterEgg] = useState(false)
+  const [clicks, setClicks] = useState(0)
+
+  useEffect(() => {
+    if (clicks === 1) {
+      setTimeout(() => {
+
+        setClicks(0)
+      }, 2000);
+    }
+    if (clicks > 10) {
+      setEasterEgg(true)
+    }
+  }, [clicks])
+
   return (
     <div
       className={`flex w-screen bg-footer text-white font-light text-[.8rem] ${isVisible ? "block" : "hidden"} mt-7 justify-center align-middle`}
     >
-      <p className="my-2 mt-3">
+      <DialogBox
+        header="Hey, you! You're finally awake."
+        content="Strona została dostarczona dla Was dzięki pracy w pocie czoła przez drużynę w składzie: Konrad Gruzek, Adam Maciuga, Mikołaj Mrózek"
+        buttonOneLabel="Super!"
+        buttonTwoLabel="Dziękuję"
+        type="DIALOG"
+        isVisible={easterEgg}
+        setVisible={setEasterEgg}
+      />
+      <p className="my-2 mt-3 select-none" onClick={() => setClicks(clicks + 1)}>
         suilo.org {new Date().getFullYear()} © Wszystkie prawa zastrzeżone.
       </p>
     </div>
