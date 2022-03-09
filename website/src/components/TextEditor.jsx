b65636ad7137570265de940b0689398b87005755
Mikołaj Mrózek <m.mik311@gmail.com>
2022-03-09 18:05:07 +0100

    started text editor

--- website/src/components/TextEditor.jsx
+++ website/src/components/TextEditor.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
-const TextEditor = () => {
+const TextEditor = ({ onChange }) => {
+    const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
+
+    useEffect(() => {
+        onChange()
+    }, [editorState])
+
     return (
-        <div >
-            text editor lol
-        </div>
-    )
+        <>
+            <Editor editorState={editorState} onChange={setEditorState} />
+        </>
+    );
 }
 
 export default TextEditor;
