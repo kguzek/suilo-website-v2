36f1f3b12743b0c92689a4f53c2da24ca1410f8d
Konrad (PC) <konrad@guzek.uk>
2022-03-12 14:01:52 +0100

    implement regex filename data extraction

--- storage_tracker/functions/index.js
+++ storage_tracker/functions/index.js
@@ -1,30 +1,50 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const { FieldValue } = require("@google-cloud/firestore");
 
-const admin = require('firebase-admin');
-
-// Private service account key 
-//Used the same one as in the api, not sure wether I should have createrd a new one
+// Private service account key
+// Used the same one as in the API, not sure whether I should have created a new one
 const serviceAccount = require("./permissions.json");
 
 // Authorise Firebase
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) },"storageTracker");
+admin.initializeApp(
+  { credential: admin.credential.cert(serviceAccount) },
+  "storageTracker"
+);
 
-const { FieldValue } = require("@google-cloud/firestore");
 admin.initializeApp();
+const db = admin.firestore();
+
+const REGEX_PATTERN = /([^\/]+?)((?<=_)\d{3,4}x\d{3,4})?(\.(?:[^\.\/]+))?$/;
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-exports.trackStorage = functions.region('europe-west1').storage.object().onFinalize(async (object) => {
-    const filePath = object.name;
-    if(filePath.startsWith("photos/")){
-        //an EXTEMLY bad way to get the real file name (would break if there is a . or a / in filename) and if the file name resembles the format blablabla_1920x1080 but it is good enought for now(hopefully)
-        const fileName = filePath.split('/')[filePath.split('/').length-1].split('.')[0];
-        const size = fileName.split('_')[fileName.split('_').length-1];
-        if(size.split('x').length !==2){
-            const addFileName = await admin.firestore().collection('_general').doc('storage').update({
-                photos: FieldValue.arrayUnion(fileName)
-            });
-        }
+exports.trackStorage = functions
+  .region("europe-west1")
+  .storage.object()
+  .onFinalize(async (object) => {
+    const match = REGEX_PATTERN.exec(object.name);
+    const [fullPath, filename, size, extension] = match;
+    if (!fullPath.startsWith("photos/")) {
+      return;
     }
-   
+    //an EXTEMLY bad way to get the real file name (would break if there is a . or a / in filename) and if the file name resembles the format blablabla_1920x1080 but it is good enought for now(hopefully)
+    //   const fileName = filePath
+    //     .split("/")
+    //     [filePath.split("/").length - 1].split(".")[0];
+    //   const size = fileName.split("_")[fileName.split("_").length - 1];
+    if (size && extension) {
+      // The file name contains the image size and file extension
+      // This means it has already been processed
+      return;
+    }
+    // Add the filename to the storage collection
+    const collectionRef = db.collection("_general");
+    await collectionRef.doc("storage").update({
+      photos: FieldValue.arrayUnion(filename),
+    });
+    // Update the "last updated" database entry
+    await collectionRef.doc("lastUpdate").update({
+      storage: admin.firestore.Timestamp.now(),
+    });
   });
