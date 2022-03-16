ae31f393c1a5ee0cbb444237b3fab5935c663e93
Mikołaj Mrózek <m.mik311@gmail.com>
2022-03-16 21:03:29 +0100

    minor visual fixes

--- website/src/components/Main/CookiesAlert.jsx
+++ website/src/components/Main/CookiesAlert.jsx
@@ -23,9 +23,8 @@ const CookiesAlert = () => {
 
   return (
     <div
-      className={`flex flex-col fixed bottom-0 align-middle justify-between p-5 pt-6 md:pt-5 bg-white w-screen rounded-t-3xl drop-shadow-2xl z-50 sm:w-11/12 md:flex-row md:w-fit md:p-6 lg:p-7 animate-all duration-300 ${
-        isOpen ? "translate-y-0" : "translate-y-full"
-      }`}
+      className={`flex flex-col fixed bottom-0 align-middle justify-between p-5 pt-6 md:pt-5 bg-white w-screen rounded-t-3xl drop-shadow-2xl z-50 sm:w-11/12 md:flex-row md:w-fit md:p-6 lg:p-7 animate-all duration-300 ${isOpen ? "translate-y-0" : "translate-y-full"
+        }`}
     >
       <div className="flex flex-col justify-center md:mr-6 align-top">
         <p className="text-text2 font-medium pb-1 text-md text-center md:text-left">
@@ -33,7 +32,7 @@ const CookiesAlert = () => {
           funkcjonowanie.
         </p>
         <p className="text-text3 font-normal text-sm pt-1 text-center pb-4 md:text-left md:pb-0">
-          psst... przechowujemy tylko informacje o sesji logowania
+          {"psst... nie przechowujemy żadnych wrażliwych danych :)"}
         </p>
       </div>
       <div className="flex flex-row-reverse md:flex-col justify-center align-middle">
