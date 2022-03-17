a8ef9ddd66c0df6baf59e8d8bb8f61e9c849bf2b
Mikołaj Mrózek <m.mik311@gmail.com>
2022-03-17 12:24:33 +0100

    another changes for build

--- website/src/index.js
+++ website/src/index.js
@@ -6,33 +6,33 @@ import { BrowserRouter } from 'react-router-dom'
 import { hydrate, render } from "react-dom";
 
 const rootElement = document.getElementById("root");
-// if (rootElement.hasChildNodes()) {
-//   hydrate(
-//     <React.StrictMode>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </React.StrictMode>,
-//     rootElement
-//   );
-// } else {
-//   render(
-//     <React.StrictMode>
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter>
-//     </React.StrictMode>,
-//     rootElement
-//   );
-// }
-render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
-);
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+    rootElement
+  );
+}
+// render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   rootElement
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
