b8ed698aed5df7fff08d29053bf41fb16b10829f
Konrad (PC) <konrad@guzek.uk>
2022-03-21 23:52:12 +0100

    add react snap to post-build scripts

--- website/src/index.js
+++ website/src/index.js
@@ -1,38 +1,26 @@
-import React from 'react';
-import './styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import "./styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import { hydrate, render } from "react-dom";
 
 const rootElement = document.getElementById("root");
+
+const toRender = [
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement,
+];
+
 if (rootElement.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-    rootElement
-  );
+  hydrate(...toRender);
 } else {
-  render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-    rootElement
-  );
+  render(...toRender);
 }
-// render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   rootElement
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
