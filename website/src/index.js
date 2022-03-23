1de493ada6d4a92b0be1ccb8fac85b1aeb0c9572
Mikołaj Mrózek <m.mik311@gmail.com>
2022-03-23 17:46:09 +0100

    serviceworkers?

--- website/src/index.js
+++ website/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { hydrate, render } from "react-dom";
+import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById("root");
 
@@ -26,3 +27,4 @@ if (rootElement.hasChildNodes()) {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// serviceWorker.unregister();
