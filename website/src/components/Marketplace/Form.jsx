d73ce7a5c8a62e4e5dacc13ce6fa331209433e31
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-06-19 16:55:29 +0200

    Created A way to Add books

--- website/src/components/Marketplace/Form.jsx
+++ website/src/components/Marketplace/Form.jsx
@@ -1,11 +1,39 @@
 import { useState, useEffect } from "react";
 import { X } from "react-feather";
+import { fetchWithToken } from "../../firebase";
+import { setErrorMessage } from "../../misc";
+
 
 const Form = ({ isOpen, closeForm, options }) => {
+  const [title, setTitle] = useState("");
+  const [studentClass, setStudentClass] = useState("1. liceum"); 
+  const [quality, setQuality] = useState("nowa");
+  const [publisher, setPublisher] = useState("");
+  const [subject,setSubject] = useState("polski");
+  const [year,setYear] = useState();
+  const [level, setLevel]  = useState("rozszerzony");
+  const [price, setPrice] = useState();
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e);
     closeForm();
+    const params = {
+      title: title,
+      studentClass: studentClass,
+      quality:quality,
+      publisher: publisher,
+      subject:subject,
+      year:year,
+      level:level,
+      price:price  
+    }
+    fetchWithToken("/books/", "POST", params).then((res) => {
+      if (res.ok) {
+        //whatever should  be done it it succeds
+      } else {
+        //whatever should be done it if fails
+      }
+    });
     //IMPLEMENTSENDING OFFER AND VALIDATING ON SERVER
   };
 
@@ -18,11 +46,11 @@ const Form = ({ isOpen, closeForm, options }) => {
       <form onSubmit={handleSubmit} className='flex flex-col'>
         <label className='flex flex-col'>
           Tytuł
-          <input placeholder='np. Biologia na czasie 1' type='text' />
+          <input placeholder='np. Biologia na czasie 1' type='text' value={title} onChange={(e)=>setTitle(e.target.value)}/>
         </label>
         <label className='flex flex-col'>
           Przedmiot
-          <select name='subject' placeholder='Wybierz z listy'>
+          <select name='subject' placeholder='Wybierz z listy' value={subject} onChange={(e)=>setSubject(e.target.value)}>
             {options.SUBJECT.map((el) => (
               <option key={el} value={el}>
                 {el}
@@ -30,17 +58,18 @@ const Form = ({ isOpen, closeForm, options }) => {
             ))}
           </select>
         </label>
+        
         <label className='flex flex-col'>
           Wydawca
-          <input placeholder='np. Nowa Era' type='text' />
+          <input placeholder='np. Nowa Era' type='text' value={publisher} onChange={(e)=>setPublisher(e.target.value)} />
         </label>
         <label className='flex flex-col'>
           Rok
-          <input placeholder='np. 2010' type='number' />
+          <input placeholder='np. 2010' type='number' value={year} onChange={(e)=>setYear(e.target.value)}/>
         </label>
         <label className='flex flex-col'>
           Poziom
-          <select name='subject' placeholder='Wybierz z listy'>
+          <select name='level' placeholder='Wybierz z listy' value={level} onChange={(e)=>setLevel(e.target.value)}>
             {options.LEVEL.map((el) => (
               <option key={el} value={el}>
                 {el}
@@ -49,8 +78,18 @@ const Form = ({ isOpen, closeForm, options }) => {
           </select>
         </label>
         <label className='flex flex-col'>
+          Klasa
+          <select name='level' placeholder='Wybierz z listy' value={studentClass} onChange={(e)=>setStudentClass(e.target.value)}>
+            {options.CLASS.map((el) => (
+              <option key={el} value={el}>
+                {el}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label className='flex flex-col'>
           Jakość
-          <select name='subject' placeholder='Wybierz z listy'>
+          <select name='quality' placeholder='Wybierz z listy'value={quality} onChange={(e)=>setQuality(e.target.value)}>
             {options.QUALITY.map((el) => (
               <option key={el} value={el}>
                 {el}
@@ -60,12 +99,7 @@ const Form = ({ isOpen, closeForm, options }) => {
         </label>
         <label className='flex flex-col'>
           Cena
-          <input type='number' />
-        </label>
-        <label className='flex flex-col'>
-          Email
-          {/* AUTO FROM USER */}
-          <input type='email' />
+          <input type='number' value={price} onChange={(e)=>setPrice(e.target.value)}/>
         </label>
         <button type='submit'>Dodaj ogłoszenie</button>
       </form>
